1de1324ee55322b4828cf5ec8f9e13622fbfeab3
Claire <cconnachan@gmail.com>
2018-02-13 22:06:05 +0000

    Added notes

--- script.rb
+++ script.rb
@@ -1,1 +1,5 @@
 #Ruby code to come.
+#So I want to input my bike rides and see what I could eat for the calories.
+#Ideally I'd like to use the Strava API but let's not get ahead of ourselves.
+#Start with a function that will take one argument - the calories burnt by one bike ride.
+#Actually, will it need two arguments? One hash with the items I could eat and one integer for the calories burned. 
